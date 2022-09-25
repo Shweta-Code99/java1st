16ce25df0abc4dd86361562fbcc12eb555a6df75
Shweta-Code99 <shwetambarisolanke@gmail.com>
2022-09-25 14:00:47 +0530

    added swp to num

---
+++ Swap2Num.java
@@ -0,0 +1,23 @@
+//write a program to swap the two numbers
+public class Swap2Num {
+	
+	public static void main(String args[]) {
+		
+		int i ,j, k;
+		i=2;
+		j=4;
+		
+		System.out.println(" i is "+i +" and j is "+j);
+		
+		k=i;
+		i=j;
+		j=k;
+		 System.out.println("i is "+ i+" and j is "+j);
+	}
+
+}
+
+
+//x=x+y; 
+//y=x-y;
+//x=x-y;
